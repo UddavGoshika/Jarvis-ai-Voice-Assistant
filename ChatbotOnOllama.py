c267bf97c9392a6eaa0822004c82a28629301a1a
Goshika Uddav <138785489+Naninetha3@users.noreply.github.com>
2025-05-01 19:22:51 +0530

    Update ChatbotOnOllama.py

--- ChatbotOnOllama.py
+++ ChatbotOnOllama.py
@@ -1,1 +1,14 @@
+import threading
+import ollama
+import sys
+import time
+import requests
+def generate_response(query):
+  answer = ollama.chat(
+    model = 'qwen2.5:1.5b',
+    message = [{'role' : 'user','content' : 'query'}]
 
+
+    
+  )
+  return answer = ['message']['content']
